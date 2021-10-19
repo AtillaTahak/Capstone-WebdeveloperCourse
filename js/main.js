638d8be1152b3c52f006f681d6cc2fcbf70a8829
Atilla Taha Kördüğüm <atillatahaa@gmail.com>
2021-10-19 19:46:12 +0300

    added linter

--- js/main.js
+++ js/main.js
@@ -1,67 +1,65 @@
-const moduleBars = document.querySelector("#mobile-menu");
-const moduleOverlay = document.querySelector(".mobile-module-overlay");
-const moduleMenu = document.querySelector(".desktop-module-nav");
-const moduleMenuTimes = document.querySelector(".modal-1");
+const moduleBars = document.querySelector('#mobile-menu');
+const moduleOverlay = document.querySelector('.mobile-module-overlay');
+const moduleMenu = document.querySelector('.desktop-module-nav');
+const moduleMenuTimes = document.querySelector('.modal-1');
 
-
-moduleBars.addEventListener('click',()=>{
-    moduleOverlay.style.display ="block";
-    moduleMenu.style.display ="block";
-
+moduleBars.addEventListener('click', () => {
+  moduleOverlay.style.display = 'block';
+  moduleMenu.style.display = 'block';
 });
-moduleMenuTimes.addEventListener('click',()=>{
-    moduleOverlay.style.display ="none";
-    moduleMenu.style.display ="none";
+moduleMenuTimes.addEventListener('click', () => {
+  moduleOverlay.style.display = 'none';
+  moduleMenu.style.display = 'none';
 });
-moduleOverlay.addEventListener('click',()=>{
-    moduleOverlay.style.display ="none";
-    moduleMenu.style.display ="none";
+moduleOverlay.addEventListener('click', () => {
+  moduleOverlay.style.display = 'none';
+  moduleMenu.style.display = 'none';
 });
 
-const speakers =[
-    {
-        id:"speaker-1",
-        image1:"./img/speaker-2.jpg",
-        names:"Serkan Şehvelioğlu",
-        jobs:"Php,mysql Software Developer in Muğla",
-        paragraph:"Serkan Şehvelioğlu he have master degree and six years have experiment",
-    },
-    {
-        id:"speaker-2",
-        image1:"./img/speaker-1.jpg",
-        names:"Berkay Dörtkardeş",
-        jobs:"Php,mysql Software Developer in Muğla",
-        paragraph:"Berkay Dörtkardeş he have master degree and six years have experiment",
+const speakers = [
+  {
+    id: 'speaker-1',
+    image1: './img/speaker-2.jpg',
+    names: 'Serkan Şehvelioğlu',
+    jobs: 'Php,mysql Software Developer in Muğla',
+    paragraph: 'Serkan Şehvelioğlu he have master degree and six years have experiment',
+  },
+  {
+    id: 'speaker-2',
+    image1: './img/speaker-1.jpg',
+    names: 'Berkay Dörtkardeş',
+    jobs: 'Php,mysql Software Developer in Muğla',
+    paragraph: 'Berkay Dörtkardeş he have master degree and six years have experiment',
 
-    },   
-     {
-        id:"speaker-2",
-        image1:"./img/speaker-1.jpg",
-        names:"Berkay Dörtkardeş",
-        jobs:"Php,mysql Software Developer in Muğla",
-        paragraph:"Berkay Dörtkardeş he have master degree and six years have experiment",
+  },
+  {
+    id: 'speaker-2',
+    image1: './img/speaker-1.jpg',
+    names: 'Berkay Dörtkardeş',
+    jobs: 'Php,mysql Software Developer in Muğla',
+    paragraph: 'Berkay Dörtkardeş he have master degree and six years have experiment',
 
-    },
-    {
-        id:"speaker-1",
-        image1:"./img/speaker-2.jpg",
-        names:"Serkan Şehvelioğlu",
-        jobs:"Php,mysql Software Developer in Muğla",
-        paragraph:"Serkan Şehvelioğlu he have master degree and six years have experiment",
-    },
-    {
-        id:"speaker-1",
-        image1:"./img/speaker-2.jpg",
-        names:"Serkan Şehvelioğlu",
-        jobs:"Php,mysql Software Developer in Muğla",
-        paragraph:"Serkan Şehvelioğlu he have master degree and six years have experiment",
-    }
+  },
+  {
+    id: 'speaker-1',
+    image1: './img/speaker-2.jpg',
+    names: 'Serkan Şehvelioğlu',
+    jobs: 'Php,mysql Software Developer in Muğla',
+    paragraph: 'Serkan Şehvelioğlu he have master degree and six years have experiment',
+  },
+  {
+    id: 'speaker-1',
+    image1: './img/speaker-2.jpg',
+    names: 'Serkan Şehvelioğlu',
+    jobs: 'Php,mysql Software Developer in Muğla',
+    paragraph: 'Serkan Şehvelioğlu he have master degree and six years have experiment',
+  },
 ];
 
-window.addEventListener('load', ()=>{
-    speakers.forEach((e)=>{
-        const element = document.createElement('future-speakers');
-        element.innerHTML =`
+window.addEventListener('load', () => {
+  speakers.forEach((e) => {
+    const element = document.createElement('future-speakers');
+    element.innerHTML = `
         <div class="speakers">
         <div class="speaker-img">
             <img src="${e.image1}" alt="">
@@ -74,5 +72,5 @@ window.addEventListener('load', ()=>{
         
     </div>`;
     document.querySelector('.program-speakers').appendChild(element);
-    });
+  });
 });
